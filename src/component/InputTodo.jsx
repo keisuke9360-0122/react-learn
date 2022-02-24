5ed31c28b4045c9c79d97a69c2e1da0b67aff895
xxxx <keitsuru0122@gmail.com>
2022-02-24 22:06:18 +0900

    3回目

---
+++ src/component/InputTodo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//(水色のインプットエリアのコンポーネント化)
+export const InputTodo = (props) => {
+  //必要な情報をpropsを通して、分割して読み込む
+  
+  const {todoText,onChange,onClick}=props
+  return (
+    <div className="input-area">
+      <input
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>追加</button>
+    </div>
+  );
+};
