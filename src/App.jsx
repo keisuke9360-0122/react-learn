7b16bc3082e4decb761c747eb7f28c06b20990da
koichi-menta <101hundredone@gmail.com>
2022-04-02 20:22:07 +0900

    sampleコードを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import { InputTodo } from "./component/InputTodo";
 import { InCompleteTodo } from "./component/IncompleteTodo";
@@ -29,6 +29,8 @@ export const App = () => {
     const newTodos = [...inCompleteTodos, todoText];
 
     setInompleteTodos(newTodos);
+
+    fetch.post({ name: "hoge" });
     seterrorTodos(false);
     setTodoText("");
   };
@@ -39,6 +41,8 @@ export const App = () => {
     console.log(newTodos);
     newTodos.splice(index, 1);
     setInompleteTodos(newTodos);
+
+    fetch.delete(2);
   };
 
   // 未完了のタスクの完了ボタンが押され、未完了のタスクが消え完了エリアに追加する処理
@@ -48,6 +52,8 @@ export const App = () => {
     setInompleteTodos(newInCompleteTodos);
     const addTodo = [...completeTodos, inCompleteTodos[index]];
     setCompleteTodos(addTodo);
+
+    fetch.put(1, { name: "fuga" });
   };
 
   //  完了エリアのタスクの戻すボタンが押され、押されたタスクが未完了エリアに追加される処理
@@ -58,6 +64,16 @@ export const App = () => {
     setInompleteTodos(backTodos);
     setCompleteTodos(newCompleteTodos);
   };
+
+  useEffect(() => {
+    fetch().then((result) => {
+      if (result.isComlete) {
+        setCompleteTodos();
+      } else {
+        setIncompleta();
+      }
+    });
+  }, []);
 
   return (
     /////何をレンダリングするか//////////
