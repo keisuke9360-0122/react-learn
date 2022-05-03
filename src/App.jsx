a586e034265ea7bd82590ad69b56aa9eb3a2454c
xxxx <keitsuru0122@gmail.com>
2022-05-03 21:22:38 +0900

    レビュー前の修正完了

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import "./index.css";
 import { InputTodo } from "./component/InputTodo";
 import { InCompleteTodo } from "./component/IncompleteTodo";
 import { CompleteTodo } from "./component/CompleteTodo";
-// import GetTodo from "./component/Todo";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-
   const [inCompleteTodos, setInompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
   const [errorTodos, seterrorTodos] = useState(false);
@@ -20,21 +18,18 @@ export const App = () => {
   };
 
   // インプットエリアから未完了エリアにテキストを追加する関数
-  const onClickAdd = (w) => {
+  const onClickAdd = () => {
     if (todoText === "") {
       const judge = true;
       seterrorTodos(judge);
       return;
     }
-    console.log(w);
-    const po = {
+    const newText = {
       name: todoText,
-      id: inCompleteTodos.length + 10,
+      id: todoText,
     };
-    const newTodos = [...inCompleteTodos, po];
-    console.log(todoText);
+    const newTodos = [...inCompleteTodos, newText];
     setInompleteTodos(newTodos);
-    console.log(newTodos);
     fetch("http://localhost:3001/todo/", {
       method: "POST",
       headers: {
@@ -42,16 +37,10 @@ export const App = () => {
       },
       body: JSON.stringify({
         name: todoText,
-        id: newTodos.length + 10,
+        id: todoText,
         inComplete: false,
       }),
-    })
-      .then((response) => response.json())
-      .then((res) => {
-        console.log(res.name);
-      });
-    // response.map((item) => console.log(item.name));
-    // レスポンス結果
+    }).then((response) => response.json());
 
     seterrorTodos(false);
     setTodoText("");
@@ -60,7 +49,6 @@ export const App = () => {
   // 未完了のエリアから削除ボタンが押された行のタスクを消す処理
   const onClickDelate = (todo, index) => {
     const newTodos = [...inCompleteTodos];
-    console.log(newTodos);
     newTodos.splice(index, 1);
     setInompleteTodos(newTodos);
     fetch(`http://localhost:3001/todo/${todo.id}`, {
@@ -70,20 +58,13 @@ export const App = () => {
       },
     });
   };
-  localStorage.setItem("newTodo", JSON.stringify(inCompleteTodos));
   // 未完了のタスクの完了ボタンが押され、未完了のタスクが消え完了エリアに追加する処理
   const onClickComplete = (todo, index) => {
     const newInCompleteTodos = [...inCompleteTodos];
-
-    const newItem = localStorage.getItem("newTodo");
-    console.log(JSON.parse(newItem));
     newInCompleteTodos.splice(index, 1);
     setInompleteTodos(newInCompleteTodos);
     const addTodo = [...completeTodos, inCompleteTodos[index]];
     setCompleteTodos(addTodo);
-    console.log("index", index);
-    console.log(todo);
-    console.log("http://localhost:3001/todo/${index}");
     fetch(`http://localhost:3001/todo/${todo.id}`, {
       method: "PUT",
       headers: {
@@ -118,32 +99,10 @@ export const App = () => {
   useEffect(() => {
     fetch("http://localhost:3001/todo/")
       .then((res) => res.json())
-      .then(
-        (result) => {
-          setCompleteTodos(result.filter((e) => e.inComplete === true));
-          setInompleteTodos(result.filter((e) => e.inComplete === false));
-        }
-        // (result) => {
-        //   console.log();
-
-        //   result.map((item) => {
-        //     if (item.inComplete === true) {
-        //       // setInompleteTodos(item);
-        //       const addTodo = [...completeTodos,item];
-        //       setCompleteTodos(addTodo);
-        //       console.log(item);
-        //     } else {
-        //       const newTodos = [...inCompleteTodos, item];
-        //       console.log(inCompleteTodos);
-        //       setInompleteTodos(newTodos);
-        //     }
-        //   });
-        //   // setInompleteTodos(result);
-        // },
-        // (error) => {
-        //   seterrorTodos(true);
-        // }
-      );
+      .then((result) => {
+        setCompleteTodos(result.filter((e) => e.inComplete === true));
+        setInompleteTodos(result.filter((e) => e.inComplete === false));
+      });
   }, []);
 
   return (
@@ -156,19 +115,12 @@ export const App = () => {
         onClick={onClickAdd}
         errorTodos={errorTodos}
       />
-      {/* <TodoData
-       onLoadTodo={myTodo}/> */}
       <InCompleteTodo
         todos={inCompleteTodos}
         onClickComplete={onClickComplete}
         onClickDelate={onClickDelate}
         // onLoadTodo={onLoadTodo}
       />
-      {/* <GetTodo
-        todos={inCompleteTodos}
-        onClickComplete={onClickComplete}
-        onClickDelate={onClickDelate}
-      /> */}
       <CompleteTodo todos={completeTodos} onClickBack={onClickBack} />
       <div></div>
     </>
